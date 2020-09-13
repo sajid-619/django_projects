18284908f53e64974ce0172ffa23974bb2fee83b
Sajid Zaman <zsajid014@gmail.com>
2020-09-13 12:19:53 +0000

    Initial Commit

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'hello.apps.HelloConfig',
     'autos.apps.AutosConfig',
     'users.apps.UsersConfig',
+    'cats.apps.CatsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -31,6 +31,7 @@ urlpatterns = [
     path('hello/', include('hello.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('autos/', include('autos.urls')),
+    path('cats/', include('cats.urls')),
     path('users/', include('users.urls')),
     path('', TemplateView.as_view(template_name='home/main.html')),
     #path('', TemplateView.as_view(template_name='hello/main.html')),
