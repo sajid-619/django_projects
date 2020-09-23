8eca36264d6e999d48a0ee7fce892ff9ba78fdcc
Sajid Zaman <zsajid014@gmail.com>
2020-09-23 12:44:11 +0000

    Ads Form

--- mysite/hello/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-
-# Create your views here.
-def hello(request) :
-    num_visits = request.session.get('num_visits', 0) + 1
-    request.session['num_visits'] = num_visits
-    if num_visits > 4 :
-        del(request.session['num_visits'])
-    return HttpResponse("view count="+str(num_visits))
